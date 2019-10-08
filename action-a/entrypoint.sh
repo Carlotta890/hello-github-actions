d980d3201239783935b40d04494bafee9783929c
Carlotta890 <54319766+Carlotta890@users.noreply.github.com>
2019-10-08 14:02:38 -0400

    Update entrypoint.sh

---
+++ action-a/entrypoint.sh
@@ -0,0 +1,8 @@
+As a part of this branch and pull request, create a file in the ./action-a/ directory titled entrypoint.sh. You can do so with this quicklink
+Add the following content to the entrypoint.sh file: 
+#!/bin/sh -l
+
+
+sh -c "echo Hello world my name is $MY_NAME"
+	Stage and commit the changes
+	Push the changes to GitHub
